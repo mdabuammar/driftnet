ae75e68d987d2f52e778dd878eff343b22e85165
Ammar <abu.ammar.cse@ulab.edu.bd>
2026-05-05 04:35:25 +0600

    fix: sync clinical options exactly with Final_Processed_Data.csv

--- frontend/src/data/clinicalData.js
+++ frontend/src/data/clinicalData.js
@@ -1,12 +1,18 @@
 // All valid dropdown options sourced directly from assets/clinical_label_mappings.json
 
 export const CLINICAL_OPTIONS = {
-  'demographic.gender': ['female', 'male'],
-
-  'demographic.vital_status': ['Alive', 'Dead'],
-
-  'samples.sample_type': ['Primary Tumor', 'Solid Tissue Normal'],
-
+  'demographic.gender': [
+    'female',
+    'male'
+  ],
+  'demographic.vital_status': [
+    'Alive',
+    'Dead'
+  ],
+  'samples.sample_type': [
+    'Primary Tumor',
+    'Solid Tissue Normal'
+  ],
   'cases.disease_type': [
     'Acinar Cell Neoplasms',
     'Adenomas and Adenocarcinomas',
@@ -15,18 +21,16 @@ export const CLINICAL_OPTIONS = {
     'Cystic, Mucinous and Serous Neoplasms',
     'Ductal and Lobular Neoplasms',
     'Epithelial Neoplasms, NOS',
-    'Squamous Cell Neoplasms',
+    'Squamous Cell Neoplasms'
   ],
-
-  'samples.tissue_type': ['Normal', 'Tumor'],
-
+  'samples.tissue_type': [
+    'Normal',
+    'Tumor'
+  ],
   'diagnoses.primary_diagnosis': [
     'Acinar cell carcinoma',
-    'Acral lentiginous melanoma, malignant',
-    'Adenocarcinoma in tubulovillous adenoma',
     'Adenocarcinoma with mixed subtypes',
     'Adenocarcinoma, NOS',
-    'Adenocarcinoma, intestinal type',
     'Adenoid cystic carcinoma',
     'Basal cell carcinoma, NOS',
     'Basaloid squamous cell carcinoma',
@@ -34,16 +38,7 @@ export const CLINICAL_OPTIONS = {
     'Bronchiolo-alveolar adenocarcinoma, NOS',
     'Bronchiolo-alveolar carcinoma, non-mucinous',
     'Carcinoma, NOS',
-    'Carcinoma, diffuse type',
-    'Cholangiocarcinoma',
     'Clear cell adenocarcinoma, NOS',
-    'Combined hepatocellular carcinoma and cholangiocarcinoma',
-    'Embryonal carcinoma, NOS',
-    'Epithelioid cell melanoma',
-    'Epithelioid mesothelioma, malignant',
-    'Hepatocellular carcinoma, NOS',
-    'Hepatocellular carcinoma, clear cell type',
-    'Hepatocellular carcinoma, fibrolamellar',
     'Infiltrating duct and lobular carcinoma',
     'Infiltrating duct carcinoma, NOS',
     'Infiltrating duct mixed with other types of carcinoma',
@@ -51,15 +46,9 @@ export const CLINICAL_OPTIONS = {
     'Intraductal papillary adenocarcinoma with invasion',
     'Invasive micropapillary carcinoma',
     'Lobular carcinoma, NOS',
-    'Malignant melanoma, NOS',
     'Medullary carcinoma, NOS',
-    'Mesothelioma, biphasic, malignant',
-    'Mesothelioma, malignant',
     'Metaplastic carcinoma, NOS',
-    'Mixed epithelioid and spindle cell melanoma',
-    'Mixed germ cell tumor',
     'Mucinous adenocarcinoma',
-    'Nodular melanoma',
     'Nonencapsulated sclerosing carcinoma',
     'Paget disease and infiltrating duct carcinoma of breast',
     'Papillary adenocarcinoma, NOS',
@@ -67,88 +56,73 @@ export const CLINICAL_OPTIONS = {
     'Papillary carcinoma, columnar cell',
     'Papillary carcinoma, follicular variant',
     'Papillary squamous cell carcinoma',
-    'Papillary transitional cell carcinoma',
     'Pleomorphic carcinoma',
     'Renal cell carcinoma, chromophobe type',
-    'Seminoma, NOS',
-    'Signet ring cell carcinoma',
     'Solid carcinoma, NOS',
-    'Spindle cell melanoma, NOS',
-    'Spindle cell melanoma, type B',
     'Squamous cell carcinoma, NOS',
     'Squamous cell carcinoma, keratinizing, NOS',
-    'Squamous cell carcinoma, large cell, nonkeratinizing, NOS',
-    'Squamous cell carcinoma, small cell, nonkeratinizing',
-    'Superficial spreading melanoma',
-    'Teratoma, benign',
-    'Teratoma, malignant, NOS',
-    'Transitional cell carcinoma',
-    'Tubular adenocarcinoma',
+    'Tubular adenocarcinoma'
   ],
-
   'diagnoses.tissue_or_organ_of_origin': [
-    'Ascending colon',
-    'Bladder, NOS',
-    'Body of stomach',
     'Breast, NOS',
-    'Cardia, NOS',
-    'Cecum',
-    'Choroid',
-    'Colon, NOS',
-    'Fundus of stomach',
-    'Gastric antrum',
-    'Head of pancreas',
-    'Hepatic flexure of colon',
-    'Intrahepatic bile duct',
     'Kidney, NOS',
-    'Larynx, NOS',
-    'Lateral wall of bladder',
-    'Liver',
-    'Lower lobe, lung',
-    'Lower third of esophagus',
-    'Lung, NOS',
-    'Middle lobe, lung',
-    'Middle third of esophagus',
-    'Overlapping lesion of lip, oral cavity and pharynx',
-    'Pancreas, NOS',
-    'Pleura, NOS',
-    'Posterior wall of bladder',
-    'Rectosigmoid junction',
-    'Rectum, NOS',
-    'Sigmoid colon',
-    'Skin, NOS',
-    'Testis, NOS',
     'Thyroid gland',
-    'Tongue, NOS',
-    'Upper lobe, lung',
+    'Upper lobe, lung'
   ],
-
   'diagnoses.morphology': [
-    '8010/3','8022/3','8050/3','8052/3','8070/3','8071/3','8072/3','8073/3',
-    '8083/3','8090/3','8120/3','8130/3','8140/3','8144/3','8145/3','8160/3',
-    '8170/3','8171/3','8174/3','8180/3','8200/3','8211/3','8230/3','8250/3',
-    '8252/3','8253/3','8255/3','8260/3','8263/3','8310/3','8317/3','8340/3',
-    '8344/3','8350/3','8480/3','8490/3','8500/3','8503/3','8507/3','8510/3',
-    '8520/3','8522/3','8523/3','8524/3','8541/3','8550/3','8575/3','8720/3',
-    '8721/3','8743/3','8744/3','8770/3','8771/3','8772/3','8774/3','9050/3',
-    '9052/3','9053/3','9061/3','9070/3','9080/0','9080/3','9085/3',
+    '8010/3',
+    '8022/3',
+    '8050/3',
+    '8052/3',
+    '8070/3',
+    '8071/3',
+    '8083/3',
+    '8090/3',
+    '8140/3',
+    '8200/3',
+    '8211/3',
+    "8230/3",
+    "8250/3",
+    "8252/3",
+    "8253/3",
+    "8255/3",
+    "8260/3",
+    "8310/3",
+    "8317/3",
+    "8340/3",
+    "8344/3",
+    "8350/3",
+    "8480/3",
+    "8500/3",
+    "8503/3",
+    "8507/3",
+    "8510/3",
+    "8520/3",
+    "8522/3",
+    "8523/3",
+    "8524/3",
+    "8541/3",
+    "8550/3",
+    "8575/3"
   ],
-
-  'diagnoses.prior_treatment': ['No', 'Yes'],
-
-  'diagnoses.prior_malignancy': ['no', 'yes'],
-
+  'diagnoses.prior_treatment': [
+    'No',
+    'Yes'
+  ],
+  'diagnoses.prior_malignancy': [
+    'no',
+    'yes'
+  ],
   'demographic.race': [
     'american indian or alaska native',
     'asian',
     'black or african american',
-    'white',
+    'white'
   ],
-
   'demographic.ethnicity': [
     'hispanic or latino',
-    'not hispanic or latino',
-  ],
+    'not hispanic or latino'
+  ]
 }
 
 // Ordered list of all 13 field keys (must match backend EXPECTED_CLINICAL_COLS order)
